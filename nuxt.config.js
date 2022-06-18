fb6db1b1b59be2d0b33b16f362ff9a81a0286235
坤仑 <lk.sun@jiangxue.team>
2022-06-19 01:23:22 +0800

    url

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    proxy: true
+    proxy: true,
+    baseURL: 'https://github-auth-demo.pages.dev'
   },
   auth: {
     strategies: {
